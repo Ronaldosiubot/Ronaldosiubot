78821640cbf70783f4377514de91bf22c96d45e0
Ronaldosiubot <150244268+Ronaldosiubot@users.noreply.github.com>
2023-11-08 17:15:00 +0530

    cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -8,7 +8,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>○ Creator : <a href='tg://user?id=760067286'>This Person</a>\n○ Language : <code>Python3</code>\n○ Source Code : N/A\n○ Channel : @Series_Movies_Paradox\n○ Support Group : @Paradox_Chatz_Group</b>",
+            text = f"<b>○ Creator : <a href='tg://user?id=760067286'>This Person</a>\n○ Owner : @Assassin069\n○ Source Code : N/A\n○ Channel : @Anime_Movies_Supernova\n○ Support Group : @Anime_Supernova_Chat</b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
